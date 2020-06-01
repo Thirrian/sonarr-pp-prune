e742b0ab782277ff7fad52e947620db39d7fd92b
Thirrian <66218321+Thirrian@users.noreply.github.com>
2020-06-01 10:38:08 +0200

    major script overhaul

--- README.md
+++ README.md
@@ -1,12 +1,15 @@
 # sonarr-pp-prune
 Sonarr postprocessing script to automatically prune series (keep only a specified number of files on disk).
-Highly untested, use at your own risk.
 
-## Usage
+**Highly untested, use at your own risk.**
+
+## Setup
+
+### Script
 
 1. Save pp-prune.sh to a directory you can access from sonarr
 
-2. Edit pp-prune-main.sh to configure it.
+2. Edit pp-prune.sh to configure it.
 
 The first section are your sonarr details and should be straightforward:
 ```bash
@@ -16,44 +19,41 @@ ip=8989
 urlbase=/sonarr # blank for typical installations
 api=your_api_key
 ```
-Secondly, set the unmonitor option to true or false. This will make an extra api call to sonarr to unmonitor an episode after deleting the corresponding file. Default: true.
+
+The second section is optional, you can specify a (debug) log file.
 ```bash
-unmonitor=true
+log="/dev/null" 
+debuglog="/dev/null"
 ```
 
-3. Create a pp-prune-x.sh script which points to `./pp-prune-main.sh <x>` where x is the number of files you want to keep. Example:
-```bash
-#!/bin/bash
-/bin/bash /path/to/prune.sh 10
-```
-4. Tag series you want to auto-prune with e.g. “prune10”
-5. Create a new Custom Script connection in sonarr:  
-  - Name: whatever you like, e.g. “Prune 10”
+### Sonarr
+
+1. Create prune tags in sonarr
+
+Create any number of tags starting with `prune-` followed by a whole number. E.g. `prune-10`.
+
+These tags are read by the script to determine how many files to keep for a particular series. If you put anything other than a whole number here (with the exception of `unmonitor`), who knows what happens.
+
+2. Optional: create `prune-unmonitor` tag in sonarr
+
+This tag is used by the script to determine if an episode should be unmonitored after a file is deleted.
+
+3. Create script connection
+
+- Go to Settings - Connect in sonarr
+- Add a new Custom Connection notification
+  - Name: anything you like
   - On Import: enable
-  - DISABLE all other events
-  - Tags: enter the same tag you used for your series, e.g. “prune10”
-  - Browse to the script you created in step 3, e.g. pp-prune-10.sh. Do NOT select the pp-prune-main.sh script.
-  
-Repeat steps 3-5 for every group of series that require a different number of files to keep.
+  - All other events (grab/upgrade/rename/health): disable
+  - Tags: not required
+  - Path: browse to pp-prune.sh
 
-## Sample script output (sonarr debug log)
+## Usage
 
-```
-20-5-31 15:08:01.9|Debug|CustomScript|Executed external script: /config/postprocessing/pp-prune_10.sh - Status: 0
-20-5-31 15:08:01.9|Debug|CustomScript|Script Output: 
-5/31/2020 1:07:54 PM - Standard - pruning The Simpsons
-5/31/2020 1:07:54 PM - Standard - files to keep : 10
-5/31/2020 1:07:54 PM - Standard - files on disk : 11
-5/31/2020 1:07:54 PM - Standard - pruning 1 file(s)...
-5/31/2020 1:07:55 PM - Standard - season  25, episode 9:
-5/31/2020 1:07:55 PM - Standard - -> file deleted
-5/31/2020 1:07:55 PM - Standard - -> episode unmonitored
-5/31/2020 1:08:01 PM - Standard - 1 file(s) pruned.
-```
+Tag series with **one** of the `prune-<x>` tags you created.
+
+Optionally tag series with `prune-unmonitor`.
 
 ## Todo
 
-Not necessarily by me. Just things I'd like to be better.
-- Turn the unmonitor option into a parameter, so it can be enabled/disabled for each prune group as required.
-- Clean up fugly code.
-- Verify if a tag is used? It will be a matter of time before someone unleashes this on their entire library because they didn't specify a tag. Idea: check if series is tagged with `prunex`. If going this route, might as well parse out the number from the tag so the script becomes generic. How to handle unmonitor flag then?
+Testing, testing, testing...
---
+++ pp-prune.sh
@@ -0,0 +1,291 @@
+#!/bin/bash
+
+# ------------------------------------
+# config
+# ------------------------------------
+
+# sonarr instance details (copy/paste from Settings > General in sonarr)
+proto=http
+host=localhost
+ip=8989
+urlbase=/sonarr
+api=ad6b514999bc435d95c5763610b467fc
+
+# script config
+# select where to save logs from the script for future reference. /dev/null discards logs
+log="/dev/null" 
+debuglog="/dev/null"
+
+# ------------------------------------
+# end config
+# ------------------------------------
+
+
+if [[ "${sonarr_eventtype}" == "Test" ]];
+then
+
+	echo "test event fired succesfully." | tee -a $log
+	exit 0
+	
+fi
+
+
+if [[ "${sonarr_eventtype}" != "Download" && "${sonarr_isupgrade}" != "False" ]];
+then
+
+	echo "this script only works for the 'On Import' event in Sonarr by design" | tee -a $log
+	echo "disable all notifications other than 'On Import' in Sonarr" | tee -a $log
+	#exit 1
+	
+fi
+
+
+# todo ${sonarr_series_id}
+
+main() {
+	
+	echo "running prune for ${sonarr_series_title}" | tee -a $log
+	
+	# get all tags from sonarr instance
+	json_sonarr_tags=$(curl -s -H "X-Api-Key: $api" $proto://$host:$ip$urlbase/api/tag | jq -r '.[] | @base64')
+	
+	# get series info
+	json_series=$(curl -s -H "X-Api-Key: $api" $proto://$host:$ip$urlbase/api/series/943)
+	
+	# fill prune_tags array
+	get_prune_tags
+	
+	# get id of prune-unmonitor tag in sonarr
+	get_unmonitor_tag_id
+	
+	# if no prune tags exist in sonarr, exit here
+	if [[ ${#prune_tags[@]} -eq 0 ]]; then
+		echo "no prune tags found, exiting" | tee -a $log
+		exit
+	fi
+	
+	# get number of files to keep, -1 in case of multiple prune tags assigned to series
+	get_files_to_keep
+	
+	# if more than one prune tag was associated with the series, exit here	
+	if [[ $files_to_keep -eq -1 ]]; then
+		echo "more than one prune tag is associated with this series" | tee -a $log
+		exit
+	fi
+	
+	# get monitored flag
+	get_monitored
+	
+	# get number of files on disk for series
+	files_available=$(echo $json_series | jq '.episodeFileCount')
+	
+	echo "files on disk : $files_available" | tee -a $log
+	echo "files to keep : $files_to_keep" | tee -a $log
+	
+	# get number of files to prune
+	files_to_prune=$((files_available-files_to_keep))
+	
+	# if no files need to be deleted, exit here
+	if [[ $files_to_prune -lt 1 ]]; then
+		echo "prune not required" | tee -a $log
+		exit
+	fi
+	
+	# get episodes with a file for this series
+	json_episodes=$(curl -s -H "X-Api-Key: $api" $proto://$host:$ip$urlbase/api/episode?seriesId=943)
+	
+	# echo "$json_episodes_with_file"
+	
+	echo "pruning $files_to_prune files..." | tee -a $log
+	
+	# delete files
+	delete_files
+	
+	# unmonitor episodes if required
+	if [[ $monitored == false ]]; then
+		unmonitor_episodes
+	fi
+	
+	echo "prune complete" | tee -a $log
+	
+}
+
+
+delete_files() {
+	
+	# list of file ids
+	file_ids=$(echo $json_episodes | jq -r '.[] | select( .hasFile ) | .episodeFileId')
+	
+	# loop over all file_ids
+	for file_id in $(echo $file_ids);
+	do
+	
+		((files_deleted+=1))
+		
+		if [[ $files_deleted -le $files_to_prune ]]; then
+			
+			delete_file
+		
+		else
+		
+			break
+			
+		fi
+		
+	done
+		
+}
+
+
+unmonitor_episodes() {
+	
+	# list of episode ids
+	episode_ids=$(echo $json_episodes | jq -r '.[] | select( .hasFile ) | .id')
+	
+	# loop over all episode_ids
+	for episode_id in $(echo $episode_ids);
+	do
+	
+		((episodes_unmonitored+=1))
+		
+		if [[ $episodes_unmonitored -le $files_to_prune ]]; then
+		
+			unmonitor_episode
+		
+		else
+		
+			break
+		
+		fi
+	
+	done
+		
+}
+
+
+delete_file() {
+	
+	# api call to delete episode file
+	curl -s -H "X-Api-Key: $api" -X DELETE $proto://$host:$ip$urlbase/api/episodefile/$file_id 1>/dev/null 2>&1
+	
+}
+
+
+unmonitor_episode() {
+	
+	# get episode json
+	episode=$(curl -s -H "X-Api-Key: $api" $proto://$host:$ip$urlbase/api/episode/$episode_id)
+	
+	# set monitored to false
+	episode=$(echo $episode | jq '.monitored = false')
+	
+	# api call to submit modified episode to disable monitoring
+	curl -s -H "X-Api-Key: $api" -H "Content-Type: application/json" -X PUT -d "$episode" $proto://$host:$ip$urlbase/api/episode 1>/dev/null 2>&1
+	
+}
+
+
+get_prune_tags() {
+	
+	# get all sonarr tags, filter out prune-* tags and fill array
+	for tag in $(echo $json_sonarr_tags);
+	do	
+		_t() {
+			echo ${tag} | base64 --decode | jq -r ${1}
+			#echo ${tag} | jq -r ${1}
+		}
+		case "$(_t '.label')" in
+			prune-unmonitor)
+				# ignore prune-unmonitor
+				;;
+			prune-*)
+				id=$(_t '.id')
+				number=$(_t '.label')
+				prune_tags+=( [$id]=${number:6} )		
+				;;
+		esac
+	done
+	
+}
+
+
+get_unmonitor_tag_id() {
+	
+	prune_unmonitor_tagid=-1
+	for tag in $(echo $json_sonarr_tags);
+	do	
+		_t() {
+			echo ${tag} | base64 --decode | jq -r ${1}
+		}
+				
+		if [[ "$(_t '.label')" == "prune-unmonitor" ]]; then
+		
+			unmonitor_tag_id=$(_t '.id')
+		
+		fi
+	done
+	
+}
+
+
+get_files_to_keep() {
+	
+	# loop over all tags for the series
+	for series_tag_id in $(echo $json_series | jq -r '.tags | .[]');
+	do
+	
+		# loop over all prune tags and match with series tags
+		for prune_tag_id in "${!prune_tags[@]}";
+		do
+		
+			# if a match for a prune-* tag is found, remember the number of files to prune and increment the counter for found prune tags
+			if [[ $series_tag_id -eq $prune_tag_id ]]; then
+								
+				((prune_tag_count+=1))
+				files_to_keep=${prune_tags[$prune_tag_id]}
+			
+			fi
+		
+		done
+
+	done
+	
+	# more than one prune tag is assigned to the series, can't find number of files to keep
+	if [[ $prune_tag_count != 1 ]]; then
+	
+		files_to_keep=-1
+	
+	fi
+	
+}
+
+
+get_monitored() {
+	
+	monitored=true
+	
+	# get unmonitor tag id, -1 when not found
+	get_unmonitor_tag_id
+	
+	# check if prune-unmonitor tag exists in sonarr
+	if [[ $unmonitor_tag_id != -1 ]]; then
+	
+		# loop over all tags for the series
+		for series_tag_id in $(echo $json_series | jq -r '.tags | .[]');
+		do
+		
+			# if prune-unmonitor tag is applied to series, set unmonitor flag to true
+			if [[ $series_tag_id == $unmonitor_tag_id ]]; then
+			
+				monitored=false
+				
+			fi
+			
+		done
+	
+	fi
+	
+}
+
+
+main
